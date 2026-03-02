51139d096e7dd011035f7d30d281369961df1790
Advait Thapliyal <advaitthapliyal@gmail.com>
2026-03-02 14:23:12 +0530

    Create 29.c

---
+++ 29.c
@@ -0,0 +1,112 @@
+/*Problem: Rotate Linked List Right by k Places - Implement using linked list with dynamic memory allocation.
+
+Input:
+- First line: integer n
+- Second line: n space-separated integers
+- Third line: integer k
+
+Output:
+- Print the linked list elements after rotation, space-separated
+
+Example:
+Input:
+5
+10 20 30 40 50
+2
+
+Output:
+40 50 10 20 30
+
+Explanation:
+Connect last node to head forming circular list. Traverse to (n-k)th node, set next to NULL, update head to (n-k+1)th node.*/
+#include <stdio.h>
+#include <stdlib.h>
+
+// Definition of Node
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to create linked list
+struct Node* createList(int n) {
+    struct Node *head = NULL, *temp = NULL, *newNode = NULL;
+
+    for (int i = 0; i < n; i++) {
+        int value;
+        scanf("%d", &value);
+
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->data = value;
+        newNode->next = NULL;
+
+        if (head == NULL) {
+            head = newNode;
+            temp = newNode;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    return head;
+}
+
+// Function to rotate right by k places
+struct Node* rotateRight(struct Node* head, int k) {
+    if (head == NULL || head->next == NULL || k == 0)
+        return head;
+
+    struct Node* temp = head;
+    int length = 1;
+
+    // Find length and last node
+    while (temp->next != NULL) {
+        temp = temp->next;
+        length++;
+    }
+
+    // Make list circular
+    temp->next = head;
+
+    // Handle k greater than length
+    k = k % length;
+    int steps = length - k;
+
+    // Move to (length - k)th node
+    struct Node* newTail = head;
+    for (int i = 1; i < steps; i++) {
+        newTail = newTail->next;
+    }
+
+    struct Node* newHead = newTail->next;
+
+    // Break circular link
+    newTail->next = NULL;
+
+    return newHead;
+}
+
+// Function to print list
+void printList(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+}
+
+int main() {
+    int n, k;
+
+    scanf("%d", &n);
+    struct Node* head = createList(n);
+
+    scanf("%d", &k);
+
+    head = rotateRight(head, k);
+
+    printList(head);
+
+    return 0;
+}
